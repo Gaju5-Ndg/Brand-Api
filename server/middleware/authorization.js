283f8874f74ad84b700c1169d6c6a770a20683e8
Gaju5-Ndg <87604337+Gaju5-Ndg@users.noreply.github.com>
2022-05-08 12:42:54 +0200

    my brand

--- server/middleware/authorization.js
+++ server/middleware/authorization.js
@@ -1,25 +1,66 @@
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
+
+// const verifyLogin = async (req, res, next) => {
+//     try{
+//         const token = req.headers.authorization.split(' ')[1];
+//         const verify = jwt.verify(token, 'testAPI');
+
+//         if(verify){
+//             req.user = verify;
+//             next();
+//         } else {
+//             res.status(401).json({
+//               status: 401,
+//               message: 'Failed to Authenticate'
+//             });
+//         }
+//     } catch(err) {
+//         res.status(401).json({
+//             status: 401,
+//             message: 'Failed to Authenticate'
+//         });
+//     }
+// }
+
+// export default verifyLogin;
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+require("core-js/modules/es.promise.js");
+
+require("core-js/modules/es.regexp.exec.js");
+
+var _jsonwebtoken = _interopRequireDefault(require("jsonwebtoken"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const verifyLogin = async (req, res, next) => {
-    try{
-        const token = req.headers.authorization.split(' ')[1];
-        const verify = jwt.verify(token, 'testAPI');
+  try {
+    const token = req.headers.authorization.split(' ')[1];
 
-        if(verify){
-            req.user = verify;
-            next();
-        } else {
-            res.status(401).json({
-              status: 401,
-              message: 'Failed to Authenticate'
-            });
-        }
-    } catch(err) {
-        res.status(401).json({
-            status: 401,
-            message: 'Failed to Authenticate'
-        });
+    const verify = _jsonwebtoken.default.verify(token, 'testAPI');
+
+    if (verify) {
+      req.user = verify;
+      next();
+    } else {
+      res.status(401).json({
+        status: 401,
+        message: 'Failed to Authenticate'
+      });
     }
-}
+  } catch (err) {
+    res.status(401).json({
+      status: 401,
+      message: 'Failed to Authenticate'
+    });
+  }
+};
 
-export default verifyLogin;
+var _default = verifyLogin;
+exports.default = _default;
